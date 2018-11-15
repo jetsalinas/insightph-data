9cec66a109c6ca166df35bdcacab4c51721557d6
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-15 23:58:43 +0800

    Modified poverty-incidence script to include headers on finished files

--- poverty-incidence/poverty-incidence.py
+++ poverty-incidence/poverty-incidence.py
@@ -1,12 +1,17 @@
 import csv
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def preprocess_file():
     data_names = ["Poverty Incidence among Population", "Magnitude of Poor Population"]
     data_poverty_incidence = []
     data_mangnitude_poor = []
+    header = None
 
     with open("poverty-incidence-per-region-psa.csv", "r") as csv_file:
         csv_reader = csv.reader(csv_file)
+        header = csv_reader.__next__()
         for row in csv_reader:
             if row[0] == data_names[0]:
                 data_poverty_incidence.append(list(row))
@@ -15,13 +20,24 @@ def preprocess_file():
 
     with open("finished/poverty-incidence-processed.csv", "w") as csv_file:
         csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(header)
         for row in data_poverty_incidence:
             csv_writer.writerow(row)
     
     with open("finished/magnitude-poor-processed.csv", "w") as csv_file:
         csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(header)
         for row in data_mangnitude_poor:
             csv_writer.writerow(row)
 
+def generate_bar_chart_poverty():
+    sns.set_style("dark")
+    f, ax = plt.subplots(figsize=(6, 15))
+    
+    poverty = pd.read_csv("finished/poverty-incidence-per-region-processed.csv")
+
+
 if __name__ == "__main__":
     preprocess_file()
+    #generate_bar_chart_poverty
+    pass
