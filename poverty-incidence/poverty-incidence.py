0bafe9c4f1737f78f6482de07f5093785b4f9e82
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-15 23:43:45 +0800

    Added script to preprocess poverty-incidence data

---
+++ poverty-incidence/poverty-incidence.py
@@ -0,0 +1,27 @@
+import csv
+
+def preprocess_file():
+    data_names = ["Poverty Incidence among Population", "Magnitude of Poor Population"]
+    data_poverty_incidence = []
+    data_mangnitude_poor = []
+
+    with open("poverty-incidence-per-region-psa.csv", "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            if row[0] == data_names[0]:
+                data_poverty_incidence.append(list(row))
+            elif row[0] == data_names[1]:
+                data_mangnitude_poor.append(list(row))
+
+    with open("finished/poverty-incidence-processed.csv", "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for row in data_poverty_incidence:
+            csv_writer.writerow(row)
+    
+    with open("finished/magnitude-poor-processed.csv", "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        for row in data_mangnitude_poor:
+            csv_writer.writerow(row)
+
+if __name__ == "__main__":
+    preprocess_file()
