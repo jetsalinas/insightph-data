e6931c5cb946422dd593a2c6a74037972cf72e7f
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-21 23:33:27 +0800

    Fixed z axis scaling in dengue

--- dengue/images.py
+++ dengue/images.py
@@ -2,9 +2,9 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def generate_heatmap_dengue():
+def generate_heatmap_dengue(year):
     dengue_long = pd.read_csv("denguecases.csv")
-    dengue_long = dengue_long.loc[dengue_long.Year == 2016]
+    dengue_long = dengue_long.loc[dengue_long.Year == year]
     dengue_long.Month = pd.to_datetime(dengue_long.Month, format='%b', errors='coerce').dt.month
     dengue = dengue_long.pivot("Month", "Region", "Dengue_Cases")
 
@@ -17,12 +17,15 @@ def generate_heatmap_dengue():
     regions = ['CAR', 'CARAGA', 'NCR', 'Region.I', 'Region.II', 'Region.III', 'Region.IV.A', 'Region.IV.B', 'Region.V', 'Region.VI', 'Region.VII', 'Region.VIII', 'Region.IX', 'Region.X', 'Region.XI', 'Region.XII', 'ARMM']
     dengue= dengue[regions]
 
+    print(dengue.max())
+
     f, ax = plt.subplots(figsize=(18, 12))
-    sns.heatmap(dengue, annot=True, linewidths=.5, ax=ax)
-    plt.title("Dengue cases per 100,000 population in the Philippines (2016)")
+    sns.heatmap(dengue, vmin=0, vmax=75, annot=True, linewidths=.5, ax=ax)
+    plt.title("Dengue cases per 100,000 population in the Philippines ({})".format(year))
     plt.xlabel("Region")
     plt.ylabel("Month")
-    plt.savefig("finished/dengue.png")
+    plt.savefig("finished/dengue-{}.png".format(year))
 
 if __name__ == "__main__":
-    generate_heatmap_dengue()
+    for i in range(2008, 2017):
+        generate_heatmap_dengue(i)
