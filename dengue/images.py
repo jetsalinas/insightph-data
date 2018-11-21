95d144be931ae14697ebc485a71fbff556cee891
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-21 22:39:55 +0800

    Fixed region ordering in dengue

--- dengue/images.py
+++ dengue/images.py
@@ -7,11 +7,15 @@ def generate_heatmap_dengue():
     dengue_long = dengue_long.loc[dengue_long.Year == 2016]
     dengue_long.Month = pd.to_datetime(dengue_long.Month, format='%b', errors='coerce').dt.month
     dengue = dengue_long.pivot("Month", "Region", "Dengue_Cases")
+
+    # order dataframe by months
     months = ['Jan', 'Feb', 'Mar', 'Apr','May','Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec']
     dengue.index = pd.Series([months[i-1] for i in dengue.index])
-    dengue.sort_index()
-    print(dengue.head())
 
+    # order dataframes by region
+    cols = dengue.columns.tolist()
+    regions = ['CAR', 'CARAGA', 'NCR', 'Region.I', 'Region.II', 'Region.III', 'Region.IV.A', 'Region.IV.B', 'Region.V', 'Region.VI', 'Region.VII', 'Region.VIII', 'Region.IX', 'Region.X', 'Region.XI', 'Region.XII', 'ARMM']
+    dengue= dengue[regions]
 
     f, ax = plt.subplots(figsize=(18, 12))
     sns.heatmap(dengue, annot=True, linewidths=.5, ax=ax)
